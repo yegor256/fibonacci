6a08fcc0583895352e4cd5c157484852df080bc5
Yegor Bugayenko <yegor256@gmail.com>
2022-01-12 09:14:23 +0300

    lambdas faster

--- src/lambdas.c
+++ src/lambdas.c
@@ -18,78 +18,74 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <stdarg.h>
 #include <stdlib.h>
 #include "./main.h"
 
-#define MAX 3
-
 struct lambda;
-typedef int (*func) (const struct lambda*[MAX]);
+typedef int (*func) (const struct lambda*);
 typedef struct lambda {
     func body;
-    int arity;
-    struct lambda* args[MAX];
+    int data;
+    struct lambda* first;
+    struct lambda* second;
+    struct lambda* third;
 } Lambda;
 
 int call(const struct lambda* l) {
     int ret;
     if (l->body == NULL) {
-        ret = l->arity;
+        ret = l->data;
     } else {
-        ret = l->body(l->args);
+        ret = l->body(l);
     }
     free(l);
     return ret;
 }
 
-Lambda* make(func body, int arity, ...) {
-    va_list argp;
-    va_start(argp, arity);
+Lambda* make(func body, struct lambda* a, struct lambda* b, struct lambda* c) {
     Lambda* l = malloc(sizeof(Lambda));
-    for (int i = 0; i < arity; ++i) {
-        l->args[i] = va_arg(argp, Lambda*);
-    }
-    va_end(argp);
     l->body = body;
-    l->arity = arity;
+    l->first = a;
+    l->second = b;
+    l->third = c;
     return l;
 }
 
 Lambda* integer(int x) {
     Lambda* l = malloc(sizeof(Lambda));
-    l->arity = x;
+    l->data = x;
     l->body = NULL;
     return l;
 }
 
-int iff(const struct lambda* args[MAX]) {
-    return call(args[0]) ? call(args[1]) : call(args[2]);
+int iff(const struct lambda* arg) {
+    return call(arg->first) ? call(arg->second) : call(arg->third);
 }
-int less(const struct lambda* args[MAX]) {
-    return call(args[0]) < call(args[1]);
+int less(const struct lambda* arg) {
+    return call(arg->first) < call(arg->second);
 }
-int sub(const struct lambda* args[MAX]) {
-    return call(args[0]) - call(args[1]);
+int sub(const struct lambda* arg) {
+    return call(arg->first) - call(arg->second);
 }
-int add(const struct lambda* args[MAX]) {
-    return call(args[0]) + call(args[1]);
+int add(const struct lambda* arg) {
+    return call(arg->first) + call(arg->second);
 }
 
-int fibo(const Lambda* args[MAX]) {
-    int x = call(args[0]);
+int fibo(const struct lambda* arg) {
+    int x = call(arg->first);
     return call(
         make(
-            iff, 3,
-            make(less, 2, integer(x), integer(2)),
+            iff,
+            make(less, integer(x), integer(2), NULL),
             integer(1),
             make(
-                add, 2,
-                make(fibo, 1, make(sub, 2, integer(x), integer(1))),
-                make(fibo, 1, make(sub, 2, integer(x), integer(2))))));
+                add,
+                make(fibo, make(sub, integer(x), integer(1), NULL), NULL, NULL),
+                make(fibo, make(sub, integer(x), integer(2), NULL), NULL, NULL),
+                NULL)));
 }
 
 int calc() {
-    return call(make(fibo, 1, integer(INPUT)));
+    return call(make(fibo, integer(INPUT), NULL, NULL));
 }
 
