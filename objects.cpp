be23fc33642e34c835bebfeab306469be86d9dad
Yegor Bugayenko <yegor256@gmail.com>
2022-01-12 04:46:16 +0300

    sa simplified

--- objects.cpp
+++ objects.cpp
@@ -1,24 +1,46 @@
-#include "metrics.h"
+/*
+ * Copyright (c) 2022 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <cstdio>
+#include "./metrics.h"
 
 class Bool {
 public:
     virtual ~Bool() = default;
-    virtual auto get() -> bool = 0;
+    virtual bool get() = 0;
 };
 
 class Int {
 public:
     virtual ~Int() = default;
-    virtual auto get() -> int = 0;
+    virtual int get() = 0;
 };
 
 class Integer : public Int {
 public:
-    explicit Integer(int v) : value(v) {};
-    explicit Integer(Int* i) : value(i->get()) {};
+    explicit Integer(int v) : value(v) {}
+    explicit Integer(Int* i) : value(i->get()) {}
     ~Integer() override = default;
-    auto get() -> int override {
+    int get() override {
         return value;
     }
 private:
@@ -27,12 +49,12 @@ private:
 
 class Less : public Bool {
 public:
-    Less(Int* l, Int* r) : left(l), right(r) {};
+    Less(Int* l, Int* r) : left(l), right(r) {}
     ~Less() override {
         delete left;
         delete right;
     }
-    auto get() -> bool override {
+    bool get() override {
         return left->get() < right->get();
     }
 private:
@@ -42,12 +64,12 @@ private:
 
 class Add : public Int {
 public:
-    Add(Int* l, Int* r) : left(l), right(r) {};
+    Add(Int* l, Int* r) : left(l), right(r) {}
     ~Add() override {
         delete left;
         delete right;
     }
-    auto get() -> int override {
+    int get() override {
         return left->get() + right->get();
     }
 private:
@@ -57,12 +79,12 @@ private:
 
 class Sub : public Int {
 public:
-    Sub(Int* l, Int* r) : left(l), right(r) {};
+    Sub(Int* l, Int* r) : left(l), right(r) {}
     ~Sub() override {
         delete left;
         delete right;
     }
-    auto get() -> int override {
+    int get() override {
         return left->get() - right->get();
     }
 private:
@@ -72,13 +94,13 @@ private:
 
 class If : public Int {
 public:
-    If(Bool* t, Int* l, Int* r) : term(t), left(l), right(r) {};
+    If(Bool* t, Int* l, Int* r) : term(t), left(l), right(r) {}
     ~If() override {
         delete term;
         delete left;
         delete right;
     }
-    auto get() -> int override {
+    int get() override {
         if (term->get()) {
             return left->get();
         }
@@ -92,11 +114,11 @@ private:
 
 class Fibo : public Int {
 public:
-    explicit Fibo(Int* v) : value(v) {};
+    explicit Fibo(Int* v) : value(v) {}
     ~Fibo() override {
         delete value;
     }
-    auto get() -> int override {
+    int get() override {
         Int* iff = new If(
             new Less(new Integer(value), new Integer(2)),
             new Integer(1),
@@ -104,21 +126,16 @@ public:
                 new Fibo(
                     new Sub(
                         new Integer(value),
-                        new Integer(1)
-                    )
-                ),
+                        new Integer(1))),
                 new Fibo(
                     new Sub(
                         new Integer(value),
-                        new Integer(2)
-                    )
-                )
-            )
-        );
+                        new Integer(2)))));
         int result = iff->get();
         delete iff;
         return result;
     }
+
 private:
     Int* value;
 };
