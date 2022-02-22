6d0122ef4f123355c7d8b3e2f06e960909b03972
Yegor Bugayenko <yegor256@gmail.com>
2022-02-22 10:15:26 +0300

    ruby

--- setup-server.sh
+++ setup-server.sh
@@ -4,3 +4,11 @@
 # self-hosted runner, where the `make.yml` GitHub action
 # must build this project.
 
+adduser --quiet --disabled-password --force-badname --gecos "" runner
+usermod -aG sudo runner
+echo "runner ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+su - runner
+
+# Then, you follow the instructions here:
+# https://github.com/yegor256/fibonacci/settings/actions/runners/new?arch=x64&os=linux
+
